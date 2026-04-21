4809fc3a4b6df69d215e13562d5efc390b2ebd24
Bullish23 <tungamiraichamisa@gmail.com>
2026-04-21 16:11:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -40,3 +40,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     checkMarketStatus();
 });
+
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav-links');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
